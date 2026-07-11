9b5ad783677ff38f6a02246a70ec44f8d4ec52de
DomHeadroom <domefemia@protonmail.com>
2026-07-11 09:54:33 +0200

    Rimosso file non utili e import non usato, migliorato iframe youtube
    Signed-off-by: DomHeadroom <domefemia@protonmail.com>

--- gatecontrol/views.py
+++ gatecontrol/views.py
@@ -1,7 +1,6 @@
 
 from django.conf import settings
-from django.http.response import  Http404, HttpResponseForbidden, \
-    HttpResponseBadRequest, JsonResponse
+from django.http.response import  Http404, HttpResponseBadRequest, JsonResponse
 from django.shortcuts import  get_object_or_404
 from rest_framework.decorators import api_view
 
