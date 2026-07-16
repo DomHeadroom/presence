bdf0a30d09acb78bd754910fd36e9f66215cd241
DomHeadroom <domefemia@protonmail.com>
2026-07-16 09:40:41 +0200

    Cambiato os.path con pathlib, tuple a array, aggiunto todo env ed escape ruff
    Signed-off-by: DomHeadroom <domefemia@protonmail.com>

--- presence/settings.py
+++ presence/settings.py
@@ -2,25 +2,26 @@
 Django settings for presence project.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
+https://docs.djangoproject.com/en/5.2/topics/settings/
 
 For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
+https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
+# Build paths inside the project like this: BASE_DIR / "subdir"
+from pathlib import Path
 
 from hlcs.gates import HpccExternal, HpccInternal
 
 
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# TODO: spostare SECRET_KEY, DEBUG e ALLOWED_HOSTS su variabili d'ambiente
 SECRET_KEY = "d((ew0%@e^l1*inklo4wb%iuirnq&&f#kp^#g-ve+n4t=x%%f*"
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -31,7 +32,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = (
+INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -40,16 +41,16 @@ INSTALLED_APPS = (
     "django.contrib.staticfiles",
     "rest_framework",
     "gatecontrol",
-)
+]
 
-MIDDLEWARE = (
+MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-)
+]
 
 SERIALIZATION_MODULES = {"yml": "django.core.serializers.pyyaml"}
 
@@ -67,7 +68,7 @@ WSGI_APPLICATION = "presence.wsgi.application"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -81,18 +82,18 @@ TEMPLATES = [
 ]
 
 # Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "NAME": BASE_DIR / "db.sqlite3",
         "ATOMIC_REQUESTS": True,
     }
 }
 
 # Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
+# https://docs.djangoproject.com/en/5.2/topics/i18n/
 
 LANGUAGE_CODE = "it-it"
 
@@ -110,11 +111,14 @@ DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
+# https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "/static/"
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+# destinazione di collectstatic in produzione (non è la sorgente static/)
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 LOGGING = {
     "version": 1,
@@ -123,7 +127,7 @@ LOGGING = {
         "file": {
             "level": "DEBUG",
             "class": "logging.FileHandler",
-            "filename": os.path.join(BASE_DIR, "presence.log"),
+            "filename": BASE_DIR / "presence.log",
             "formatter": "verbose",
         },
         "console": {
@@ -160,6 +164,6 @@ GATES = {
 
 
 try:
-    from .local_settings import *
+    from .local_settings import *  # noqa: F403
 except ImportError:
     pass
